060bc3692d046aac42444e2a32fa4d61c7533600
Harkishore Singh <harkishoresingh@gmail.com>
2026-09-20 14:12:21 +0530

    Update API_BASE in DashboardView and HistoricalTrends

--- app-code/axion-ui/src/components/pages/DashboardView.tsx
+++ app-code/axion-ui/src/components/pages/DashboardView.tsx
@@ -7,7 +7,7 @@ import { Throughput } from '../Throughput';
 import { DigitalTwin } from '../DigitalTwin';
 import { ThermalCamera } from '../ThermalCamera';
 
-const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://telemetry.hypertrace.online';
 
 interface DashboardViewProps {
   devices: any[];
